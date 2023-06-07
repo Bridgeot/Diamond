3dec5a6fb38f842bb61054e5a1fb1c46e35894bf
Bridgeot <bridgeot789@gmail.com>
2023-06-07 16:49:55 -0400

    setup initial structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/material'
+import './App.css'
+
+import { Navbar, Footer } from './components'
+import { Home, About, Talent, Ranking, Reviews } from './pages'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width='400px' sx={{ width: { xl: '1488px' }}} m='auto'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Talent' element={<Talent />} />
+        <Route path='/Ranking' element={<Ranking />} />
+        <Route path='/Reviews' element={<Reviews />} />
+      </Routes>
+      <Footer />
+    </Box>
   )
 }
 
