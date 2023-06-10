10bc24c9594433912fb4e6b7bf3b5b34d1b5fb50
Bridgeot <bridgeot789@gmail.com>
2023-06-10 19:38:38 -0400

    update routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { Box } from '@mui/material'
 import './App.css'
 
 import { Navbar, Footer } from './components'
@@ -8,17 +7,17 @@ import { Home, About, Talent, Ranking, Reviews } from './pages'
 
 const App = () => {
   return (
-    <Box width='400px' sx={{ width: { xl: '1488px' }}} m='auto'>
+    <div className='App'>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/About' element={<About />} />
-        <Route path='/Talent' element={<Talent />} />
-        <Route path='/Ranking' element={<Ranking />} />
-        <Route path='/Reviews' element={<Reviews />} />
+        <Route path='#about' element={<About />} />
+        <Route path='#talent' element={<Talent />} />
+        <Route path='#ranking' element={<Ranking />} />
+        <Route path='#reviews' element={<Reviews />} />
       </Routes>
       <Footer />
-    </Box>
+    </div>
   )
 }
 
