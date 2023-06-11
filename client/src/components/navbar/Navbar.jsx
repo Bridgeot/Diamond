0b5b66c3ca4ffa18ab4e39918fef56531d86a5f0
Bridgeot <bridgeot789@gmail.com>
2023-06-10 20:05:02 -0400

    add navbar content and styles

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './navbar.css'
+
+import Logo from '../../assets/diamond-logo.png'
+
+const linkStyle = {
+  color: 'var(--secondary-color)',
+  fontFamily: 'var(--gilda-display)',
+  fontSize: '1.25rem',
+  lineHeight: '1.25rem',
+  textTransform: 'uppercase',
+  cursor: 'pointer',
+}
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='navbar-logo'>
+          <img src={Logo} alt='diamond logo' />
+      </div>
+
+      <div className='navbar-menu'>
+          <Link to='/' style={linkStyle}>Home</Link>
+          <Link to='#about' style={linkStyle}>About</Link>
+          <Link to='#talent' style={linkStyle}>Talent</Link>
+          <Link to='#ranking' style={linkStyle}>Ranking</Link>
+          <Link to='#reviews' style={linkStyle}>Reviews</Link>
+      </div>
+    </div>
   )
 }
 
