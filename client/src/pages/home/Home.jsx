5f647be386db419a535fb255bae870caadda3511
Bridgeot <bridgeot789@gmail.com>
2023-06-10 20:36:03 -0400

    add home content and styles

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './home.css'
 
+import HomeImg from '../../assets/home-img.png'
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='home-image'>
+        <img src={HomeImg} alt='cute girl with long brown hair smiling back at you' />
+      </div>
+
+      <div className='home-content'>
+        <div className='home-content-info'>
+          <h1 className='gradient-color'>A Diamond <br /> in the Rough</h1>
+          <p>We're Japan's largest "rental girlfriend" service located in the heart <br /> of Tokyo. To get started, please schedule your first date down below.</p>
+
+          <div className='home-content-btns'>
+            <button className='home-content-btn1' type='button'>Rent Now</button>
+            <button className='home-content-btn2' type='button'>Learn More</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
