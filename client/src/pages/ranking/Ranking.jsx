d014040f61fe138b0aa21a284fdb92c6dab0ca8b
Bridgeot <bridgeot789@gmail.com>
2023-06-11 10:50:04 -0400

    add ranking content and styles

--- client/src/pages/ranking/Ranking.jsx
+++ client/src/pages/ranking/Ranking.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import Content from '../../components/content/Content'
 import './ranking.css'
 
 const Ranking = () => {
   return (
-    <div>Ranking</div>
+    <div className='ranking'>
+      <div className='ranking-heading'>
+        <h2 className='gradient-color'>Ranking System</h2>
+      </div>
+
+      <div className='ranking-content-container'>
+        <div className='ranking-content'>
+          <Content title={ <span className='ranking-title'>This Week</span> } />
+          <Content title={ <span className='ranking-gold'>01.</span> } text='Maki Suzuki' />
+          <Content title={ <span className='ranking-silver'>02.</span> } text='Sara Boston' />
+          <Content title={ <span className='ranking-bronze'>03.</span> } text='Layla Osaka' />
+        </div>
+        <div className='ranking-content'>
+          <Content title={ <span className='ranking-title'>This Month</span> } />
+          <Content title={ <span className='ranking-gold'>01.</span> } text='Sara Boston' />
+          <Content title={ <span className='ranking-silver'>02.</span> } text='Layla Osaka' />
+          <Content title={ <span className='ranking-bronze'>03.</span> } text='Maki Suzuki' />
+        </div>
+        <div className='ranking-content'>
+          <Content title={ <span className='ranking-title'>This Year</span> } />
+          <Content title={ <span className='ranking-gold'>01.</span> } text='Sara Boston' />
+          <Content title={ <span className='ranking-silver'>02.</span> } text='Maki Suzuki' />
+          <Content title={ <span className='ranking-bronze'>03.</span> } text='Layla Osaka' />
+        </div>
+      </div>
+    </div>
   )
 }
 
