e2059092ad822a53f8077975196fc2d6b41701f3
Bridgeot <bridgeot789@gmail.com>
2023-06-12 09:15:54 -0400

    try to add reviews crud feature

--- client/src/pages/reviews/Reviews.jsx
+++ client/src/pages/reviews/Reviews.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './reviews.css'
 
 const Reviews = () => {
+  const [reviews, setReviews] = useState(null);
+
+  useEffect(() => {
+    fetchReviews();
+  }, [])
+
+  const fetchReviews = async () => {
+    const res = await axios.get('http://localhost:3001/reviews');
+
+    console.log(res);
+    setReviews(res.data.reviews);
+    console.log(res);
+  };
+
   return (
-    <div>Reviews</div>
+    <div className='reviews'>
+      <div className='reviews-heading'>
+        <h2 className='gradient-color'>Leave a Review</h2>
+      </div>
+
+      <div>
+        <h2>Reviews:</h2>
+        {reviews && 
+          reviews.map((review) => {
+            return (
+              <div key={review._id}>
+                <h3>{review.title}</h3>
+              </div>
+            );
+          })}
+      </div>
+    </div>
   )
 }
 
