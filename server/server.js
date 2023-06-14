fcb877d8f16cf55d11873cef8984ee747ded4139
Bridgeot <bridgeot789@gmail.com>
2023-06-13 20:18:13 -0400

    update dependecies, middleware, and routes

--- server/server.js
+++ server/server.js
@@ -1,19 +1,32 @@
+// require dependencies
 const express = require('express');
+const review = require('./controllers/review');
 const cors = require('cors');
-const reviewController = require('./controllers/review');
 
+// initialize express application
 const app = express();
 
-app.use(cors());
-
+// configure application settings
 require('dotenv').config();
 require('./config/database');
 
-app.get('/reviews', reviewController.fetchReviews);
-app.get('/reviews/:id', reviewController.fetchReview);
-app.post('/reviews', reviewController.createReview);
-app.delete('/reviews/:id', reviewController.deleteReview);
+// mount middlware
+app.use(express.json());
+app.use(cors());
 
+// mount routes
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+
+app.get('/reviews', review.fetchReviews);
+app.get('/reviews/:id', review.fetchReview);
+app.post('/reviews', review.createReview);
+app.put('/reviews/:id', review.updateReview);
+app.delete('/reviews/:id', review.deleteReview);
+
+// tell the application to listen for requests
 const port = process.env.PORT || 3001;
 app.listen(port, function() {
     console.log(`Express app is running on port:${port}`);
