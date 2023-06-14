90c640f9f3da3eb6110cbe38fe67a267cd2f9d7e
Bridgeot <bridgeot789@gmail.com>
2023-06-13 23:10:57 -0400

    fix content placement and add styling

--- client/src/pages/reviews/Reviews.jsx
+++ client/src/pages/reviews/Reviews.jsx
@@ -6,6 +6,7 @@ const Reviews = () => {
   const [reviews, setReviews] = useState(null);
   const [createReviewForm, setCreateReviewForm] = useState({
     title: '',
+    rating: '',
     body: '',
   });
 
@@ -35,7 +36,7 @@ const Reviews = () => {
 
     setReviews([...reviews, res.data.review]);
 
-    setCreateReviewForm({ title: '', body: '' });
+    setCreateReviewForm({ title: '', rating: '', body: '' });
   };
 
   const deleteReview = async (_id) => {
@@ -54,38 +55,42 @@ const Reviews = () => {
         <h2 className='gradient-color'>Leave a Review</h2>
       </div>
 
-      <div>
-        <h2>Reviews:</h2>
-        {reviews && 
-          reviews.map((review) => {
-            return (
-              <div key={review._id}>
-                <h3>{review.title}</h3>
-                <button onClick={() => deleteReview(review._id)}>
-                  Delete Note
-                </button>
-              </div>
-            );
-          })}
-      </div>
+      <div className='reviews-content-container'>
+        <div className='reviews-content'>
+          <h2>Reviews:</h2>
+          {reviews && 
+            reviews.map((review) => {
+              return (
+                <div className='reviews-content-info' key={review._id}>
+                  <h3>{review.title}</h3>
+                  <p>{review.rating}</p>
+                  <p>{review.body}</p>
+                  <div className='reviews-content-info-btn'>
+                    <button onClick={() => deleteReview(review._id)}>
+                      Delete Note
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
 
-      <div>
-        <h2>Create Review:</h2>
-        <form onSubmit={createReview}>
-          <input 
-            onChange={updateCreateReviewForm} 
-            name='title' 
-            value={createReviewForm.title} 
-            type='text' 
-          />
-          <textarea 
-            onChange={updateCreateReviewForm} 
-            name='body' 
-            value={createReviewForm.body} 
-            cols='30' rows='10'>
-          </textarea>
-          <button type='submit'>Create Review</button>
-        </form>
+        <div className='reviews-content'>
+          <h2>Create Review:</h2>
+          <form className='reviews-content-form' onSubmit={createReview}>
+            <input onChange={updateCreateReviewForm} name='title' value={createReviewForm.title} type='text' />
+            <select onChange={updateCreateReviewForm} name='rating' value={createReviewForm.rating} >
+              <option value='' selected>Enter a rating</option>
+              <option value='&#9733;&#9733;&#9733;&#9733;&#9733;'>5 Star</option>
+              <option value='&#9733;&#9733;&#9733;&#9733;'>4 Star</option>
+              <option value='&#9733;&#9733;&#9733;'>3 Star</option>
+              <option value='&#9733;&#9733;'>2 Star</option>
+              <option value='&#9733;'>1 Star</option>
+            </select>
+            <textarea onChange={updateCreateReviewForm} name='body' value={createReviewForm.body} type='text' cols='30' rows='10'></textarea>
+            <button type='submit'>Add Review</button>
+          </form>
+        </div>
       </div>
     </div>
   )
